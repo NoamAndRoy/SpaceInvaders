8c15f8b0819e2caebbed0c1be2b6559ded09dfec
Me <Me@DESKTOP-SUG6NI3>
2016-08-14 18:45:25 +0300

    Add class Wall

---
+++ SpaceInvaders/Models/Wall.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Models;
+using Microsoft.Xna.Framework;
+
+namespace SpaceInvaders.Models
+{
+    public class Wall : CollideableSprite
+    {
+        public Wall(Game i_Game, string i_TexturePath) 
+            : base(i_Game, i_TexturePath)
+        {
+        }
+    }
+}
